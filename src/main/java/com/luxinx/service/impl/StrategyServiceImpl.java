845d2bebd265012d8d294233b13c652f9e05bc14
javalusir <javalusir@163.com>
2018-02-17 16:18:42 +0800

    deal days must be bigger than stocklist size in StrategyServiceImpl

--- src/main/java/com/luxinx/service/impl/StrategyServiceImpl.java
+++ src/main/java/com/luxinx/service/impl/StrategyServiceImpl.java
@@ -18,47 +18,47 @@ public class StrategyServiceImpl implements StrategyService {
 
     @Override
     public Map<String, String> choiceavgStock(String code,String days) {
-        Set<Map<String,String>> result = new HashSet<>();
-        String sqlavg = "select closeprice,vol,tn.stockname from tb_stock_history th,tb_stock_name tn  where th.stockcode=tn.stockid and stockcode='" + code + "' order by datestr DESC limit "+days;
-        List<Map<String, Object>> listprice = dao.executeQuery(sqlavg);
-        if (listprice == null || listprice.isEmpty()) {
-            return new HashMap<>();
-        }
-        double vollast = 0;
-        double volbeforelast = 0;
-        double trend = 0;  //判断股票趋势方向
-        String stockname = "";
-        double avgprice = 0.0;
-        double currprice = 0.0;
-        if (listprice != null && listprice.size() > 2) {
-            vollast = Double.parseDouble(listprice.get(0).get("vol") + "");
-            volbeforelast = Double.parseDouble(listprice.get(1).get("vol") + "");
-            trend = (vollast / volbeforelast);
-            stockname = listprice.get(0).get("stockname") + "";
-            currprice = ((BigDecimal)listprice.get(0).get("closeprice")).doubleValue();
-        }
-
-        final BigDecimal[] total = {new BigDecimal(0)};
-        total[0].setScale(2);
-        listprice.forEach((Map<String, Object> e) -> {
-            total[0] = total[0].add(new BigDecimal(e.get("closeprice") + ""));
-        });
-        BigDecimal biglistsize = new BigDecimal(listprice.size());
-
-        log.info("当前"+currprice+"："+stockname + ":"+days+"avgPrice: " + total[0].divide(biglistsize, 2, BigDecimal.ROUND_DOWN));
-        avgprice = total[0].divide(biglistsize, 2, BigDecimal.ROUND_DOWN).doubleValue();
-
         Map<String, String> stockmap = new HashMap<>();
-        //判断成交量比前一日放量才获取日均线
-        if (trend > 0&&currprice>avgprice) {
-            stockmap.put(code, avgprice+"");
-            result.add(stockmap);
-//            String deletesql = "delete from tb_stock_focus where stockcode='" + code + "'";
-//            dao.execute(deletesql);
-//            String insertfocus = "insert into tb_stock_focus (stockcode,stockname,destprice,updown,issend,datecreated)values('" + code + "','" + stockname + "','" + price25avg + "',1,0,NOW())";
-//            dao.execute(insertfocus);
-//            log.info(insertfocus);
+        try {
+            String sqlavg = "select closeprice,vol,tn.stockname from tb_stock_history th,tb_stock_name tn  where th.stockcode=tn.stockid and stockcode=? order by datestr DESC limit ?";
+            List<Map<String, Object>> listprice = dao.executeQuery(sqlavg,new Object[]{code,days});
+            if (listprice == null || listprice.isEmpty()||listprice.size()<Integer.parseInt(days)) {
+                return new HashMap<>();
+            }
+            double vollast;
+            double volbeforelast;
+            double trend = 0;  //判断股票趋势方向
+            String stockname = "";
+            double avgprice;
+            double currprice = 0.0;
+            if (listprice != null && listprice.size() > 2) {
+                vollast = Double.parseDouble(listprice.get(0).get("vol") + "");
+                volbeforelast = Double.parseDouble(listprice.get(1).get("vol") + "");
+                trend = (vollast / volbeforelast);
+                stockname = listprice.get(0).get("stockname") + "";
+                currprice = ((BigDecimal)listprice.get(0).get("closeprice")).doubleValue();
+            }
+
+            final BigDecimal[] total = {new BigDecimal(0)};
+            total[0].setScale(2);
+            listprice.forEach((Map<String, Object> e) -> {
+                total[0] = total[0].add(new BigDecimal(e.get("closeprice") + ""));
+            });
+            BigDecimal biglistsize = new BigDecimal(listprice.size());
+
+            log.info("当前"+currprice+"："+stockname + ":"+days+"avgPrice: " + total[0].divide(biglistsize, 2, BigDecimal.ROUND_DOWN));
+            avgprice = total[0].divide(biglistsize, 2, BigDecimal.ROUND_DOWN).doubleValue();
+
+
+            //判断成交量比前一日放量才获取日均线
+            if (trend > 0&&currprice>avgprice) {
+                stockmap.put(code, avgprice+"");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            return stockmap;
         }
-        return stockmap;
     }
 }
